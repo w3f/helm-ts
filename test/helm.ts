9ef15a93abcb3cb240f9b6f17f294c6cd61934f5
Federico Gimenez <federico.gimenez@gmail.com>
2020-05-23 16:24:26 +0200

    start kind and initialize k8s client

--- test/helm.ts
+++ test/helm.ts
@@ -4,12 +4,19 @@ import path from 'path';
 import tmp from 'tmp';
 import { Components } from '@w3f/components';
 import { Cmd } from '@w3f/cmd';
+import { Kind } from '@w3f/kind';
+import * as k8s from '@kubernetes/client-node';
 
 import { Helm } from '../src/index';
+import { ChartConfig } from '../src/types';
 import {
     LoggerMock,
     TplMock
 } from './mocks';
+
+should();
+
+const kc = new k8s.KubeConfig();
 
 const logger = new LoggerMock();
 const tpl = new TplMock();
@@ -22,8 +29,7 @@ const cm = new Components('helm-test', cmCfg, logger);
 const cmd = new Cmd(logger);
 
 let subject: Helm;
-
-should();
+let kind: Kind;
 
 describe('Helm', () => {
     before(async () => {
@@ -31,10 +37,28 @@ describe('Helm', () => {
         const kindPath = await cm.path('kind');
 
         subject = new Helm(helmPath, cmd, tpl, logger);
+
+        kind = new Kind(kindPath, cmd, logger);
+        await kind.start();
+
+        const kubeconfigContent = await kind.kubeconfig() as string;
+
+        kc.loadFromString(kubeconfigContent);
+    });
+
+    after(async () => {
+        await kind.stop();
     });
 
     describe('install', () => {
-        it('should install a local chart');
+        it('should install a local chart', async () => {
+            const chart = path.join(__dirname, 'test');
+            const chartCfg: ChartConfig = {
+                name: 'test',
+                chart
+            };
+
+        });
         it('should install a remote chart from stable');
         it('should install a remote chart from a custom repo');
         it('should allow to pass values');
