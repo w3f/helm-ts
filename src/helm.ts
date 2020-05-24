11fb624cd8493b8209b572e1740580cd82953d3e
Federico Gimenez <federico.gimenez@gmail.com>
2020-05-24 20:17:15 +0200

    basic install chart and make test pass

--- src/helm.ts
+++ src/helm.ts
@@ -1,21 +1,44 @@
+import { CmdManager } from '@w3f/cmd';
 import { Logger } from '@w3f/logger';
 import { TemplateManager } from '@w3f/template';
-import { CmdManager } from '@w3f/cmd';
 
-import { HelmManager, ChartConfig } from './types';
+import { HelmManager, HelmConfig, ChartConfig } from './types';
 
 export class Helm implements HelmManager {
-    constructor(
-        private readonly binPath: string,
-        private readonly cmd: CmdManager,
-        private readonly tpl: TemplateManager,
-        private readonly logger: Logger) { }
+    private readonly binaryPath: string;
+    private readonly cmd: CmdManager;
+    private readonly kubeconfig: string;
+    private readonly tpl: TemplateManager;
+    private readonly logger: Logger
 
-    install(chart: ChartConfig): Promise<void> {
-        return
+    constructor(helmCfg: HelmConfig) {
+        this.binaryPath = helmCfg.binaryPath;
+        this.cmd = helmCfg.cmd;
+        this.kubeconfig = helmCfg.kubeconfig;
+        this.tpl = helmCfg.tpl;
+        this.logger = helmCfg.logger;
+
+        this.cmd.setOptions({ verbose: true });
     }
 
-    uninstall(name: string): Promise<void> {
-        return
+    async install(chartCfg: ChartConfig): Promise<string> {
+        const result = await this.exec(
+            'upgrade',
+            chartCfg.name,
+            chartCfg.chart,
+            '--install',
+            '--kubeconfig',
+            this.kubeconfig,
+            '--wait'
+        ) as string;
+        return result;
+    }
+
+    async uninstall(name: string): Promise<string> {
+        return '';
+    }
+
+    private async exec(...args: string[]): Promise<string | number> {
+        return this.cmd.exec(`${this.binaryPath}`, ...args);
     }
 }
