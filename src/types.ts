422c3f637edb713559e178395e5e8f177b95a588
Federico Gimenez <federico.gimenez@gmail.com>
2020-05-26 10:25:28 +0200

    repository related types

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,13 @@ import { CmdManager } from '@w3f/cmd';
 import { Logger } from '@w3f/logger';
 import { TemplateManager } from '@w3f/template';
 
+export interface Repo {
+    name: string;
+    url: string;
+}
+
+export type RepoList = Array<Repo>;
+
 export interface ChartValuesTemplate {
     path: string;
     data: string;
@@ -26,4 +33,5 @@ export interface ChartConfig {
 export interface HelmManager {
     install(chart: ChartConfig): Promise<string>;
     uninstall(name: string): Promise<string>;
+    addRepos(repos: RepoList): Promise<void>;
 }
