f1f0207243d12e41a42c29b2abd813c4d9fa9dcb
ironoa <alessio.onori@gmail.com>
2021-03-05 11:03:16 +0100

    updated downloaded helm version

--- test/helm.ts
+++ test/helm.ts
@@ -19,7 +19,7 @@ const kc = new k8s.KubeConfig();
 const logger = createLogger();
 
 const cmCfg = {
-    'helm': 'https://w3f.github.io/components-ts/downloads/linux-amd64/helm/3.2.1/helm.tar.gz',
+    'helm': 'https://w3f.github.io/components-ts/downloads/linux-amd64/helm/3.5.2/helm.tar.gz',
     'kind': 'https://w3f.github.io/components-ts/downloads/linux-amd64/kind/0.7.0/kind.tar.gz'
 };
 const cm = new Components('helm-test', cmCfg, logger);
