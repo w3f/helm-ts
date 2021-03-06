ae848fc5af04724e581383ad493202dd2e5afe60
ironoa <alessio.onori@gmail.com>
2021-03-06 09:53:17 +0100

    new kind version

--- test/helm.ts
+++ test/helm.ts
@@ -20,7 +20,7 @@ const logger = createLogger();
 
 const cmCfg = {
     'helm': 'https://w3f.github.io/components-ts/downloads/linux-amd64/helm/3.5.2/helm.tar.gz',
-    'kind': 'https://w3f.github.io/components-ts/downloads/linux-amd64/kind/0.7.0/kind.tar.gz'
+    'kind': 'https://w3f.github.io/components-ts/downloads/linux-amd64/kind/0.10.0/kind.tar.gz'
 };
 const cm = new Components('helm-test', cmCfg, logger);
 const cmd = new Cmd(logger);
